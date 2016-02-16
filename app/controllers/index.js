3082d9a8d7051db39c97f8df8a620eb4d92b758f
Alejandro <alejandroslpz@gmail.com>
2016-02-16 11:04:31 -0600

    Jsonapp con elementos que lleva

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,9 +10,38 @@ var json;
 var xhr = Ti.Network.createHTTPClient({
 	onload: function(e)	{
 		json = JSON.parse(this.responseText);
+		//alert(json);
 		Ti.API.info(this.responseText);
 		JSON.stringify(json);
-	},
+		//alert(JSON.stringify(json));
+	//Ciclo for para la creacion de los segmentos
+	for(var i=0; i<json.items.length; i++){
+			
+			//creacion de datos
+			var row = Ti.UI.createTableViewRow({
+				
+				datos:json.items[i],
+			
+			});
+			
+			//Donde se va a ver
+			var view = Ti.UI.createView({
+						
+			});	
+			
+			//label que va a llevar cada seccion		
+			var titulo = Ti.UI.createLabel({
+						
+			});	
+			
+			//imagen que va a llevar cada seccion		
+			var imagen = Ti.UI.createImageView({
+						
+						
+			});
+
+	};
+},
 	
 	onerror: function(e) {
 		Ti.API.info(e.error);
@@ -24,7 +53,4 @@ var xhr = Ti.Network.createHTTPClient({
 xhr.open('GET', url);
 xhr.send();
 
-for(var i=0,j=json.items.length; i<j; i++){
-	var row = Ti.UI.
-  datos:json.items[i];
-};
+//$.tabla.setData(datos);
